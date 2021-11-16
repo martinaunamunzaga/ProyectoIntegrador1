dba42b24d04a1c7ab499b58cabdc5ae3d2c4b76c
martumoroni <80982224+martumoroni@users.noreply.github.com>
2021-11-16 19:08:05 -0300

    registracion

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,28 @@
-const posts = require (`../data/data`)
+const db = require("../database/models")
+const bcrypt = require("bcryptjs")
+const validateUser = function (req) {
+  const errors = [];
+
+  if (!req.file) {
+    errors.push('LA IMAGEN ES REQUERIDA');
+  }
+  if (!req.body.contraseña || req.body.contraseña.length < 3) {
+    errors.push('LA CONSTRASEÑA NO PUEDE ESTAR VACÍA, NI SER MENOR A 3 CARACTERES');
+  }
+  if (!req.body.nombre) {
+    errors.push('EL NOMBRE ES REQUERIDO');
+  }
+  if (!req.body.email) {
+    errors.push('EL EMAIL ES REQUERIDO');
+  }
+  if (!req.body.nacimiento) {
+    errors.push('LA FECHA ES REQUERIDA');
+  }
+  if (!req.body.telefono) {
+    errors.push('EL TELÉFONO ES REQUERIDO');
+  }
+  return errors;
+}
 const controller = {
   miPerfil:  function(req, res) {
       for (let i = 0; i < posts.list.length; i++) {
@@ -12,6 +36,21 @@ const controller = {
       registracion: function(req, res) {
         res.render('registracion');
       },
+      store: function(req, res) {
+        let errores = validateUser(req)
+        if (errores.length > 0) {
+          return res.render('registracion', { errores });
+        }
+        req.body.contraseña = bcrypt.hashSync(req.body.contraseña, 10);
+        req.body.imagen = (req.file.destination + req.file.filename).replace('public', '')
+        db.Usuario.create(req.body)
+          .then(post => {
+            res.redirect('/users/login');
+          }).catch(error => {
+            return res.render(error);
+          })
+        res.render('registracion');
+      },
       login: function (req, res){
         res.render ('login');
       },
@@ -19,7 +58,5 @@ const controller = {
         res.render ('editarPerfil');
       }
 }
-
-
 
 module.exports = controller
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/usersController');
+var multer = require('multer');
+const upload = multer({ dest: 'public/images/' });
+
 /* GET users listing. */
 router.get('/profile/:id', usersController.miPerfil);
 router.get('/register', usersController.registracion);
 router.get ('/login', usersController.login) 
 router.get ('/edit', usersController.editarPerfil)
+router.post (`/register`, upload.single(`imagen`), usersController.store)
 
 module.exports = router;
